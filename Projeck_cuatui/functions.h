95ebc2f21817ad285d4b834899d448a8287a9910
nguyenanhtuan335 <nguyenthoithoi0@gmail.com>
2025-01-08 10:41:34 +0700

    Update functions.h

--- Projeck_cuatui/functions.h
+++ Projeck_cuatui/functions.h
@@ -1,7 +1,10 @@
 #include "dataTypes.h"
+
 void roleMenu();
 void login();
 void manageMenu();
+
+//student
 void studentMenu();
 void inputStudent(Student student[], int *length);
 void printStudent(Student student[], int length);
@@ -9,7 +12,6 @@ void editStudent(Student student[], int length);
 void backAndExit();
 void deleteStudent(Student student[], int *length);
 void sortStudent(Student student[], int length);
-void saveStudentToFile(Student student[], int length);
-void loadStudentFromFile(Student student[], int *length);
+void saveStudentToFile(Student student[], int length, int a);
+void loadStudentFromFile(Student student[], int *length, int a);
 void searchStudent(Student student[], int length);
-
