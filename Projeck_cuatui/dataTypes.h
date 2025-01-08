f9b03db83fafc73100ed328164d314ba1d864a3a
nguyenanhtuan335 <nguyenthoithoi0@gmail.com>
2025-01-08 10:41:00 +0700

    Update dataTypes.h

--- Projeck_cuatui/dataTypes.h
+++ Projeck_cuatui/dataTypes.h
@@ -2,16 +2,8 @@
 typedef struct{
 	int studentId;
 	char classroomId[10];
-	char name[30];
-	char email[35];
+	char name[25];
+	char email[30];
 	char phoneNumber[15];
 	
 }Student;
-
-typedef struct{
-	int teacherId;
-	char classroomId[10];
-	char name[30];
-	char email[35];
-	char phoneNumber[15];
-}Teacher;
